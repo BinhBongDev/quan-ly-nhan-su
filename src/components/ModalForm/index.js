83ce2f2741ff973acd95363cfbd16555ca28c9b7
BinhBongDev <hoangvanbinht37@gmail.com>
2022-11-07 21:29:53 +0700

    20. validate formModal

--- src/components/ModalForm/index.js
+++ src/components/ModalForm/index.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Button, Modal, ModalHeader, ModalBody, ModalFooter, Form, Row, FormGroup, Label, Input, Col } from 'reactstrap';
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter, Form, Row, FormGroup, Label, Input, Col, FormFeedback } from 'reactstrap';
 import { v4 as uuidv4 } from 'uuid';
 import {useDispatch} from 'react-redux'
 
@@ -8,31 +8,35 @@ import { idToDept } from '../../utils/configDept';
 import dateFormat from 'dateformat';
 
 function ModalForm({toggleModal, idStaff}) {
-    let formatDob
-    let formatStart
-    if(idStaff) {
-        formatDob = dateFormat(idStaff.doB, "yyyy-mm-dd")
-        formatStart = dateFormat(idStaff.startDate, "yyyy-mm-dd")
+    const initStaff = {
+      id:idStaff?.id || uuidv4(),
+      name:idStaff?.name || '',
+      doB: (idStaff && dateFormat(idStaff?.doB, "yyyy-mm-dd")) || '',
+      startDate: (idStaff && dateFormat(idStaff.startDate, "yyyy-mm-dd")) || '',
+      image: "/assets/images/alberto.png",
+      overTime: idStaff?.overTime || 1,
+      annualLeave: idStaff?.annualLeave || 1,
+      salaryScale: idStaff?.salaryScale     || 1.2,
+      departmentId: idStaff?.departmentId || 'Dept01'
     }
-  const [modal, setModal] = useState(true);
-  const initStaff = {
-    id:idStaff?.id || uuidv4(),
-    name:idStaff?.name || '',
-    doB: formatDob || '',
-    startDate: formatStart || '',
-    image: "/assets/images/alberto.png",
-    overTime: idStaff?.overTime || 1,
-    annualLeave: idStaff?.annualLeave || 1,
-    salaryScale: idStaff?.salaryScale     || 1.2,
-    departmentId: idStaff?.departmentId || 'Dept01'
-  }
-  const [staff, setStaff] = useState(initStaff)
+    const touched = {
+        name: false,
+        doB: false,
+        startDate: false,
+        annualLeave: false,
+        salaryScale: false,
+        overTime: false
+    }
+    const [focus, setFocus] = useState(touched)
+    const [modal, setModal] = useState(true);
+    const [staff, setStaff] = useState(initStaff)
+    const [fullInput, setFullInput] = useState(false)
 
-  const toggle = () => {
-    setModal(!modal)
-    toggleModal(false)
+    const toggle = () => {
+        setModal(!modal)
+        toggleModal(false)
 
-  };
+    };
   const handleChangeValue = (e) => {
     const name = e.target.name
     const value = e.target.value
@@ -48,15 +52,76 @@ function ModalForm({toggleModal, idStaff}) {
 
   const submitForm = async(e) => {
     e.preventDefault()
-    if(idStaff) {
-        dispatch(editStaff(staff))
-        
+    if(name.length < 3 || name.length > 20 || !doB || !startDate || !overTime || !annualLeave || !salaryScale) {
+       return setFullInput(true)
     } else {
-        dispatch(postStaff(staff))
+        if(idStaff) {
+            dispatch(editStaff(staff))
+            
+        } else {
+            dispatch(postStaff(staff))
+        }
+        toggle()
+
     }
-    toggle()
   }
+
+  const handleBlur = (field) => {
+    setFocus({
+        ...focus,
+        [field]: true
+    })
+  }
+  
+//   validate 
+  const validate = (name, doB, startDate, overTime, salaryScale, annualLeave) => {
+    const errors = {
+        name: '',
+        doB: '',
+        startDate: '',
+        overTime: '',
+        salaryScale: '',
+        annualLeave: ''
+    }
+
+   
+    if(focus.name && name.length < 3)
+        errors.name = 'Min leng is > 3 characters.'
+    else if(focus.name && name.length > 20)
+        errors.name = "Max leng is <= 20 characters."
+    else if(fullInput & !name) {
+        errors.name = 'Fill input'
+    }
+
+    if (focus.doB && !doB)
+        errors.doB = 'input required. '
+    else if(fullInput && !doB) {
+        errors.doB = 'Fill input'
+    }
+
+    if (focus.startDate && !startDate)
+        errors.startDate = 'input required. '
+    else if(fullInput && !startDate) {
+        errors.startDate = 'Fill input'
+    }
+
+    if (focus.overTime && !overTime)
+        errors.overTime = 'input required. '
+    
+    if (focus.salaryScale && !salaryScale)
+        errors.salaryScale = 'input required. '
+
+    if (focus.annualLeave && !annualLeave)
+        errors.annualLeave = 'input required. '
+
+
+    return errors
+  }
+
   const {name, doB, startDate, salaryScale, annualLeave, overTime, departmentId} = staff
+//   erros
+  const errors = validate(name, doB, startDate, overTime, salaryScale, annualLeave)
+  
   return (
     <div>
       <Modal isOpen={modal} toggle={toggle}>
@@ -73,7 +138,13 @@ function ModalForm({toggleModal, idStaff}) {
                         placeholder="Enter your name"
                         value={name}
                         onChange={handleChangeValue}
+                        onBlur={() =>handleBlur('name')}
+                        valid = {errors.name === ''}
+                        invalid = {errors.name !== ''}
                         />
+                        <FormFeedback>
+                            {errors.name}
+                        </FormFeedback>
                 </FormGroup>
                 <Row>
                     <Col md={6}>
@@ -87,7 +158,13 @@ function ModalForm({toggleModal, idStaff}) {
                                 type="date"
                                 value={doB}
                                 onChange={handleChangeValue}
-                            />
+                                onBlur={() => handleBlur('doB')}
+                                valid = {errors.doB === ''}
+                                invalid = {errors.doB !== ''}
+                                />
+                                <FormFeedback>
+                                    {errors.doB}
+                                </FormFeedback>
                         </FormGroup>
                     </Col>
                     <Col md={6}>
@@ -101,7 +178,13 @@ function ModalForm({toggleModal, idStaff}) {
                             type="date"
                             value={startDate}
                             onChange={handleChangeValue}
+                            onBlur={() => handleBlur('startDate')}
+                            valid = {errors.startDate === ''}
+                            invalid = {errors.startDate !== ''}
                             />
+                            <FormFeedback>
+                                {errors.startDate}
+                            </FormFeedback>
                         </FormGroup>
                     </Col>
                 </Row>
@@ -149,9 +232,16 @@ function ModalForm({toggleModal, idStaff}) {
                             name="overTime"
                             placeholder="examp: 1, 1.5, ..."
                             type='number'
+                            step={0.5}
                             value={overTime}
                             onChange={handleChangeValue}
+                            onBlur={() => handleBlur('overTime')}
+                            valid = {errors.overTime === ''}
+                            invalid = {errors.overTime !== ''}
                             />
+                            <FormFeedback>
+                                {errors.overTime}
+                            </FormFeedback>
                         </FormGroup>
                     </Col>
                     <Col md={4}>
@@ -165,8 +255,15 @@ function ModalForm({toggleModal, idStaff}) {
                             placeholder="examp: 1, 1.5, ..."
                             type='number'
                             value={salaryScale}
+                            step={0.01}
                             onChange={handleChangeValue}
+                            onBlur={() => handleBlur('salaryScale')}
+                            valid = {errors.salaryScale === ''}
+                            invalid = {errors.salaryScale !== ''}
                             />
+                            <FormFeedback>
+                                {errors.salaryScale}
+                            </FormFeedback>
                         </FormGroup>
                     </Col>
                     <Col md={4}>
@@ -180,8 +277,17 @@ function ModalForm({toggleModal, idStaff}) {
                                 placeholder=""
                                 type='number'
                                 value={annualLeave}
+                                min={1}
+                                max={12}
+                                step={0.5}
                                 onChange={handleChangeValue}
+                                onBlur={() => handleBlur('annualLeave')}
+                                valid = {errors.annualLeave === ''}
+                                invalid = {errors.annualLeave !== ''}
                                 />
+                                <FormFeedback>
+                                    {errors.annualLeave}
+                                </FormFeedback>
                             </FormGroup>
                     </Col>
                 </Row>
